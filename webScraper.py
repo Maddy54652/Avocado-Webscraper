6139f12e6fc1bb10ef20bf393454e062db837047
Maddy <37761862+Maddy54652@users.noreply.github.com>
2024-07-16 22:19:01 -0700

    created getURL function for user input

--- webScraper.py
+++ webScraper.py
@@ -5,15 +5,21 @@ import re
 
 
 def introScreen():
+    #print new line
     print('Hello! Welcome to the Avocado Webscraper. This is version 1.0.1 Created by Maddy Henderson')
 
+def getURL():
+    #ask for url
+    userURL = input('please enter the url of the webpage you would like to obtain:')
+    return userURL
+    #return user input
 
 
 introScreen()
 #The header may need to be changed depending on your chrome browser
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
 #URL can be changed
-URLtoObtain = "https://www.sfml-dev.org/tutorials/2.5/window-window.php"
+URLtoObtain = getURL()
 
 #getting the html
 myHumblestOfRequests = requests.get(URLtoObtain,headers = headers)
